ced8a49cfb82fe8c855e7c1707bb0b0c380a2697
augustohanashiro <49253803+augustohanashiro@users.noreply.github.com>
2023-09-18 17:21:28 -0300

    apagando_try_except

--- Codigos/try_except.py
+++
@@ -1,7 +0,0 @@
-numero = input("Digite um numero:")
-
-try:
-    numero_float = int(numero)
-    print(f"O dobro de {numero} é {numero_float*2}")
-except:
-    print("A informação digitada não é um número")
